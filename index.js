97438adf97c1cd01e0874979ca144776df740ee9
jupegarnica <jupegarnica@gmail.com>
2019-03-27 11:07:01 +0100

    /register endpoint read body in json

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ const app = express();
 const port = +process.argv[2] || 3000;
 
 
+// body parse to json
+app.use(express.json());
+
+
 app.get('/hola', (request, response) => {
-    console.log('alguien hecho una petición Get a /hola')
-    let miResponse = { code: 200, data: 'Adios mundo cruel'}
-    response.json( miResponse )
+    console.log('alguien hecho una petición Get a /hola');
+    let miResponse = { code: 200, data: 'Adios mundo cruel'};
+    response.json( miResponse );
 })
 
 
-app.listen(port , () => console.log('servidor escuchando en el puerto ' + port))
+
+app.post('/register', (req,res) => {
+
+    console.log(req.body);
+    res.status(201).json({data:'created!'});
+
+} );
+
+
+app.listen(port , () => console.log('servidor escuchando en el puerto ' + port));

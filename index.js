260445251d4852245d9f2ba54f725bd95792b639
jupegarnica <jupegarnica@gmail.com>
2019-03-27 12:35:54 +0100

    /register working with all cases handled

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const id = require( 'uniqid' );
+var validator = require( 'validator' );
+
 // const app = require('express')();
 const port = +process.argv[2] || 3000;
 
@@ -17,11 +21,65 @@ app.get('/ping', (request, response) => {
 
 
 app.post('/register', (req,res) => {
+    let {username, email, password} = req.body;
 
-    console.log(req.body);
-    res.status(201).json({data:'created!'});
+    if (username && email && password) {
+
+        if (password.length < 8) {
+            res.status(400).json({message:'El password debe ser tener al menos 8 caracteres'})
+            return;
+        }
+        try {
+            // get text from file
+            const dataString = fs.readFileSync( './db.json', 'UTF-8' );
+            // parse json string to object
+            var data = JSON.parse( dataString );
+
+        } catch (error) {
+             res.status( 500 ).json( { message: 'Upss, algo ha ido mal. Perdón a sido mi culpa. Inténtelo de nuevo más tarde' } )
+             return;
+        }
+
+        if (data.users.find(user => user.username === username)) {
+            res.status( 400 ).json( { message: 'El nombre de usuario ya existe, elija otro' } )
+            return;
+        }
+
+         if ( data.users.find( user => user.email === email ) ) {
+             res.status( 400 ).json( { message: 'Ya existe un usuario con ese email. Puede logearse' } )
+             return;
+         }
+
+         if ( !validator.isEmail( email ) ) {
+             res.status( 400 ).json( { message: 'El email no es un email. Escríbalo bien melón' } )
+             return;
+         }
+
+         const newUser = {
+             id: id(),
+             username,
+             email,
+             password
+         }
+
+         //  update object
+         data.users.push( newUser );
+         // transform object to string
+         const outputString = JSON.stringify( data )
+         // write string to file
+         fs.writeFileSync( './db.json', outputString )
+
+         // response
+         res.status( 201 ).json( { data: 'created!' } );
+
+
+    } else {
+        res.status(400).json({message: 'necesitamos un body con las propiedades username, email y password'})
+    }
+
+
 
 } );
 
 
-app.listen(port , () => console.log('servidor escuchando en el puerto ' + port));
+app.listen(port , () => console.log('servidor escuchando en el puerto ' + port))
